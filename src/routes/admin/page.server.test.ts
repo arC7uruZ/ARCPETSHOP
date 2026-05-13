78bd675d08a202a0d7fc5bd2ea9d3e51f47cfda5
Welton Carvalho <welton.carvalho.dev@gmail.com>
2026-05-13 14:32:50 -0300

    test: create unit testes for /admin/+page.server.ts

---
+++ src/routes/admin/page.server.test.ts
@@ -0,0 +1,35 @@
+import { describe, vi, expect, it } from "vitest";
+
+vi.mock("$lib/server/admin.server", () => {
+	return {
+		fetchAdminStats: vi.fn(),
+		fetchRecentAppointments: vi.fn()
+	};
+});
+
+import {
+	fetchAdminStats,
+	fetchRecentAppointments
+} from "$lib/server/admin.server";
+
+import { load } from "./+page.server";
+
+const mockLocals = {
+    supabase: "mock",
+}
+
+describe("+page.server.ts load function", () => {
+    it("should return stats and recentAppointments", async () => {
+        vi.mocked(fetchAdminStats).mockResolvedValue("adminStats" as any)
+        vi.mocked(fetchRecentAppointments).mockResolvedValue("recentAppointments" as any)
+        const result = await load({ locals: mockLocals } as never)
+
+        expect(fetchAdminStats).toHaveBeenCalledOnce();
+        expect(fetchRecentAppointments).toHaveBeenCalledOnce();
+        expect(result).toStrictEqual({
+            stats: "adminStats",
+            recentAppointments: "recentAppointments",
+        });
+    })
+})
+
